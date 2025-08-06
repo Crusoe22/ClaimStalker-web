bbecb0d5adbd3cc1c0dc5132d20f7809f58a7f2b
Nolan Moss <nolanmmoss@gmail.com>
2025-08-06 13:42:36 -0400

    replace bycrypt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { UserCollection, ClaimCollection } = require("./config"); // Import collections as per your config.js file
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require("nodemailer");
 
 const app = express();
