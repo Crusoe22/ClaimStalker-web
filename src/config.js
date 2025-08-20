0cfc7b27c62d093ae8bfcd35f7473c06d19b286d
Nolan Moss <nolanmmoss@gmail.com>
2025-08-20 19:23:45 -0400

    Update database connection

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/Login-tut")
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Database Connected Successfully");
     })
