bf76a0df4337ffaf0f49690cc0d0d753862418b8
Nolan Moss <nolanmmoss@gmail.com>
2025-11-18 20:12:52 -0500

    update to new model

--- models/customer_submitted_claims.js
+++ models/customer_submitted_claims.js
@@ -1,3 +1,4 @@
+/*
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define("customer_submitted_claims", {
     claim_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
@@ -16,4 +17,27 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     timestamps: true
   });
+};*/
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("CustomerSubmittedClaims", {
+        claim_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        policy_number: { type: DataTypes.STRING, allowNull: false },
+        first_name: { type: DataTypes.STRING, allowNull: false },
+        last_name: { type: DataTypes.STRING, allowNull: false },
+        email: { type: DataTypes.STRING, allowNull: false },
+        phone: { type: DataTypes.STRING, allowNull: false },
+        claim_date: { type: DataTypes.DATEONLY, allowNull: false },
+        address_1: { type: DataTypes.TEXT, allowNull: false },
+        address_2: { type: DataTypes.TEXT },
+        state: { type: DataTypes.STRING },
+        zip_code: { type: DataTypes.STRING },
+        description: { type: DataTypes.TEXT, allowNull: false },
+        photo_urls: { type: DataTypes.ARRAY(DataTypes.STRING) }
+    }, {
+        tableName: "customer_submitted_claims",
+        timestamps: true,            // enable timestamps
+        createdAt: "created_at",     // map to existing column
+        updatedAt: "updated_at"      // map to existing column
+    });
 };
