a962b451071f5cea5f23071f8dacc245a04fbb94
Nolan Moss <nolanmmoss@gmail.com>
2025-12-06 15:12:32 -0500

    add search by like for policy number

--- routes/viewClaims.js
+++ routes/viewClaims.js
@@ -9,7 +9,7 @@ router.get("/view-claim", async (req, res) => {
   try {
     let where = {};
     if (policyNumber) where.policyNumber = policyNumber;
-    else if (searchType === 'policyNumber') where.policyNumber = searchValue;
+    else if (searchType === 'policyNumber') where.policyNumber = { [Op.iLike]: `%${searchValue}%` }; //else if (searchType === 'policyNumber') where.policyNumber = searchValue;
     else if (searchType === 'name') where.name = { [Op.iLike]: `%${searchValue}%` }; // search by "like" value
     else if (searchType === 'email') where.email = { [Op.iLike]: `%${searchValue}%` }; // search by "like" value
     else if (searchType === 'phone') where.phone = { [Op.iLike]: `%${searchValue}%` }; // search by "like" value 
