caf7d36f8c9744079c0f733723a0f6af00036113
Nolan Moss <nolanmmoss@gmail.com>
2025-12-07 19:37:03 -0500

    add new bucketname

--- routes/tempPhotoLink.js
+++ routes/tempPhotoLink.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+// Initialize S3 Client (new code)
+const {S3Client, GetObjectCommand} = require("@aws-sdk/client-s3");
+const client = new S3Client{ClientParams};
+
+
 
 const s3 = new S3Client({ region: "us-east-1" });
 
@@ -11,7 +16,7 @@ router.get("/download/:fileKey", async (req, res) => {
     const fileKey = req.params.fileKey;
 
     const command = new GetObjectCommand({
-      Bucket: process.env.AWS_BUCKET,
+      Bucket: process.env.BUCKET_NAME,//AWS_BUCKET,
       Key: fileKey,
     });
 
