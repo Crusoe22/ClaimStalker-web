3d81f7dcfb829070b928e3a6b974a585ca6ffa47
Nolan Moss <nolanmmoss@gmail.com>
2025-12-07 20:19:11 -0500

    try new photo download

--- routes/tempPhotoLink.js
+++ routes/tempPhotoLink.js
@@ -16,24 +16,36 @@ const s3 = new S3Client({
   region: process.env.BUCKET_REGION,
 })
 
+
+function extractKey(fullUrl) {
+  // remove { } if they exist
+  fullUrl = fullUrl.replace(/^\{|\}$/g, "");
+
+  // remove domain, bucket, region
+  const parts = fullUrl.split(".amazonaws.com/");
+  return parts[1]; // <-- returns only the key
+}
+
 //const s3 = new S3Client({ region: "us-east-1" });
 
-router.get("/download/*", async (req, res) => {
+router.get("/download", async (req, res) => {
   try {
-    const fileKey = req.params.fileKey;
+    const fullUrl = req.query.url;  // from your frontend
+    const fileKey = extractKey(fullUrl);
 
     const command = new GetObjectCommand({
       Bucket: process.env.BUCKET_NAME,
       Key: fileKey,
     });
 
-    const url = await getSignedUrl(s3, command, { expiresIn: 86400 });
-    res.redirect(url);
+    const signed = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    res.redirect(signed);
 
   } catch (err) {
     console.error(err);
-    res.status(500).send("Cannot create download link.");
+    res.status(500).send("Error creating download link");
   }
 });
+
 
 module.exports = router;
