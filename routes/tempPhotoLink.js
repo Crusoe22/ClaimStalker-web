88074e273cf7986f80b2c85eb584f813993ef65c
Nolan Moss <nolanmmoss@gmail.com>
2025-12-07 19:59:54 -0500

    temp photo

--- routes/tempPhotoLink.js
+++ routes/tempPhotoLink.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 // Initialize S3 Client (new code)
 //const {S3Client, GetObjectCommand} = require("@aws-sdk/client-s3");
 //const client = new S3Client{ClientParams};
 
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+  region: process.env.BUCKET_REGION,
+})
 
+//const s3 = new S3Client({ region: "us-east-1" });
 
-const s3 = new S3Client({ region: "us-east-1" });
-
-router.get("/download/:fileKey", async (req, res) => {
+router.get("/download/*", async (req, res) => {
   try {
     const fileKey = req.params.fileKey;
 
     const command = new GetObjectCommand({
-      Bucket: process.env.BUCKET_NAME,//AWS_BUCKET,
+      Bucket: process.env.BUCKET_NAME,
       Key: fileKey,
     });
 
